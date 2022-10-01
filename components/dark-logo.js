720f5101d18d83a9433b835a45ffdb1951fd7f59
256thFission <103920583+256thFission@users.noreply.github.com>
2022-09-30 22:50:43 -0700

    Vercel version 9

--- components/dark-logo.js
+++ components/dark-logo.js
@@ -3,7 +3,8 @@ import Image from 'next/image';
 
 function DarkLogo() {
   return (
-    <Link href="/pages">
+    <Link href="/index.jsx">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="inline-block align-middle leading-[1]">
         <Image
           src="/images/chez.png"
