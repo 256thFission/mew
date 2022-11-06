1fb308d0555d3da260875410ec006be18778e1a3
256thFission <103920583+256thFission@users.noreply.github.com>
2022-11-05 21:04:41 -0700

    Prod 5.2

--- components/dark-logo.js
+++ components/dark-logo.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 function DarkLogo() {
   return (
-    <Link href="/index.jsx">
+    <Link href="/">
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="inline-block align-middle leading-[1]">
         <Image
